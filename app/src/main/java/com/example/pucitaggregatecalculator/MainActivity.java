dd7a24e8317ed3945fdc1d42c85a7ab1edcbc082
imnaeem <ceinfo.pk@gmail.com>
2021-10-24 22:59:57 +0500

    Erros

--- app/src/main/java/com/example/pucitaggregatecalculator/MainActivity.java
+++ app/src/main/java/com/example/pucitaggregatecalculator/MainActivity.java
@@ -17,43 +17,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    Button Calculate;
     TextView textView;
+    EditText editText;
     double matric_marks=0;
     double inter_marks=0;
-    double aggregate=0.0;
+    double calculate=0.0;
 
     boolean hafiz_quran=false;
 
 
     public void onCheckboxClicked(View view) {
-        CheckBox simpleCheckBox = (CheckBox) findViewById(R.id.checkBox);
-        Boolean checkBoxState = simpleCheckBox.isChecked();
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkBox:
+                if (checked)
+                hafiz_quran = true;
+        else
+            hafiz_quran = false;
 
-        if(checkBoxState)
-        {
-            hafiz_quran = true;
+                break;
+
         }
     }
 
     public void Calculate(View view) {
-        textView = (EditText) findViewById (R.id.matricmarks);
-        matric_marks = Double.parseDouble(textView.getText().toString());
-        textView = (EditText) findViewById (R.id.intermediatemarks);
-        inter_marks = Double.parseDouble(textView.getText().toString());
+        editText = (EditText) findViewById (R.id.matricmarks);
+        matric_marks = Double.parseDouble(editText.getText().toString());
+        editText = (EditText) findViewById (R.id.intermediatemarks);
+        inter_marks = Double.parseDouble(editText.getText().toString());
 
-        if (hafiz_quran)
-        {
-            inter_marks = inter_marks +20;
-        }
-        matric_marks = (matric_marks*30)/100;
-        inter_marks = (inter_marks*70)/100;
-
-        aggregate = matric_marks + inter_marks;
-
-        textView = textView.findViewById(R.id.aggregate);
-
-        textView.setText(Double.toString(aggregate));
+//        if (hafiz_quran == true)
+//        {
+//            inter_marks = inter_marks +20;
+//        }
+//        matric_marks = (matric_marks*30)/100;
+//        inter_marks = (inter_marks*70)/100;
+//
+//        calculate = matric_marks + inter_marks;
+//
+//        editText = editText.findViewById(R.id.aggregate);
+//
+//        editText.setText(Double.toString(calculate));
 
     }
 }
